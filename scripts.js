be027f5c2fc5de98ab51fd66ecdcb5d02dc35fe9
Diego-Gomes84 <goedi.gomes@gmail.com>
2023-04-14 22:44:00 -0300

    adding faq

--- scripts.js
+++ scripts.js
@@ -5,7 +5,6 @@ a.addEventListener('mouseover', changebgcolor)
 a.addEventListener('mouseout', nobgcolor)
 
 function highlight(){
-    
     a.style.color = 'red'
 }
 function changebgcolor(){
@@ -18,3 +17,17 @@ function nobgcolor(){
     a.style.color = ''
     
 }
+
+let li = document.querySelectorAll(".faq-text li");
+    for (var i = 0; i < li.length; i++) {
+      li[i].addEventListener("click", (e)=>{
+        let clickedLi;
+        if(e.target.classList.contains("question-arrow")){
+          clickedLi = e.target.parentElement;
+        }else{
+          clickedLi = e.target.parentElement.parentElement;
+        }
+       clickedLi.classList.toggle("showAnswer");
+      });
+    }
+
